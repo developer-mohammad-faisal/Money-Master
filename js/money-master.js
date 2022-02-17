3af596d2ef79a5e388cfd9c1f91f08270574685a
Your Name <you@example.com>
2022-02-17 21:50:55 +0600

    added

--- js/money-master.js
+++ js/money-master.js
@@ -1,14 +1,65 @@
 
+function getInput(inputId){
+  const inputField = document.getElementById(inputId);
+  const inputValue = parseInt(inputField.value);
+  inputField.value = '';
+  return inputValue;
+}
+
+document.getElementById('calculate-btn').addEventListener('click',function(){
+    const income = getInput('income-input')
+    const food = getInput('food-input')
+    const rent = getInput('rent-input')
+    const clothes = getInput('clothes-input')
+    if(isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(clothes)){
+            alert('Please Type The Number')
+    }
+    else if( income <0 || food <0 || rent <0 || clothes <0 ){
+      alert ('Please Write The Possitive Number')
+    }
+    else{
+       const totalBalance = food + rent + clothes;
+       if(income < totalBalance){
+          alert ("Please Earn Money")
+       }
+       else {
+        document.getElementById("total-expenses").innerText = totalBalance;
+        document.getElementById("total-balance").innerText = income - totalBalance;
+        
+       }
+      
+    }
+})
+
+// Saving Button
+document.getElementById('save-btn').addEventListener('click', function(){
+   const savingField = getInput('save-input').innerText;
+   
+   
+})
+
+
+
+
+
+
+
+
+
+
+/* 
 
 function incomeCalculate(){
   const foodInput = document.getElementById('food-input');
   const rentInput = document.getElementById('rent-input');
   const clothesInput = document.getElementById('clothes-input');
   const totalAmount = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothesInput.value);
+
   return totalAmount;
+
 }
 
-// Salculate Button 
+// Calculate Button 
 document.getElementById('calculate-btn').addEventListener('click',function(){
 
       // total expenses
@@ -18,7 +69,6 @@ document.getElementById('calculate-btn').addEventListener('click',function(){
       document.getElementById('total-balance').innerText = incomeInputText;
 
 })
-
 
 
 
@@ -41,4 +91,4 @@ document.getElementById('save-btn').addEventListener('click',function(){
 
 })
 
-
+ */
